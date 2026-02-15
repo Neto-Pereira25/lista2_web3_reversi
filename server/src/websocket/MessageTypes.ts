2d0d2b65d37dbbd3ea2c8bb69fb6b360e8ba89ff
José Pereira da Silva Neto <devneto203@gmail.com>
2026-02-15 01:29:52 -0300

    update: update the types of websocket messages

--- server/src/websocket/MessageTypes.ts
+++ server/src/websocket/MessageTypes.ts
@@ -1,6 +1,8 @@
 export type ClientMessage =
+    | { type: "JOIN_ROOM"; payload: { roomId: string } }
     | { type: "MOVE"; payload: { row: number; col: number } };
 
 export type ServerMessage =
+    | { type: "WELCOME"; payload: any }
     | { type: "STATE"; payload: any }
     | { type: "ERROR"; payload: string };
